cc9a2e0e5aaa814ab1e15fc01f131cb5aa77a5a9
Sergey Kostrukov <sergey@kostrukov.com>
2026-02-15 07:55:29 -0800

    Typing fixes

--- packages/moexr-client/src/moexr/client/__init__.py
+++ packages/moexr-client/src/moexr/client/__init__.py
@@ -1,5 +1,24 @@
-from .client import MoexClient
+from .client import MoexClient, Query
 from .error import MoexClientError, PaginationError
 from .pagination import DatePagination, LimitOnly, OffsetPagination, Pagination
 from .properties import PropertyValue, to_properties
 from .table import ColumnMetadataEntry, IndexT, MoexIndexedTable, MoexTable, Row, Value
+
+__all__ = [
+    "ColumnMetadataEntry",
+    "DatePagination",
+    "IndexT",
+    "LimitOnly",
+    "MoexClient",
+    "MoexClientError",
+    "MoexIndexedTable",
+    "MoexTable",
+    "OffsetPagination",
+    "Pagination",
+    "PaginationError",
+    "PropertyValue",
+    "Query",
+    "Row",
+    "Value",
+    "to_properties",
+]
